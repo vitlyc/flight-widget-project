f2884f8c8773dfaa19141ab237797f1b1dbddb05
Vitaly <72764133+vitlyc@users.noreply.github.com>
2022-10-15 13:37:53 +0300

    update

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ app.listen(PORT, () => console.log(`running on port ` + PORT))
 
 app.get("/flights", (req, res) => {
   const options = {
-    url: "https://83b89beb-968a-4eb1-ae6e-1110837008ff-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/flights/collections/departures",
+    url: process.env.URL,
     headers: {
       accept: "application/json",
-      "X-Cassandra-Token":
-        "AstraCS:SSbsEWhZXORUrIhWZaQmmWRX:3290016673232f75c5ac1261e76f5256c13d07cedf92af89181060c24b51f9e4",
+      "X-Cassandra-Token": process.env.TOKEN,
     },
   }
   axios
